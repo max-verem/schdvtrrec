92916e725be3d68c7c3e5304547acc4f8ebb31f0
verem@INTERNAL.M1STEREO.TV <verem@INTERNAL.M1STEREO.TV@607f2e03-de62-4924-8c59-f6f35288d6ba>
2007-12-10 17:56:19 +0000

    draft schedule monitoring

--- trunk/src/SchdVtrRecIni.h
+++ trunk/src/SchdVtrRecIni.h
@@ -13,16 +13,31 @@
 
 class CSchdVtrRecIni  
 {
+
 public:
+	int f_exit;
+
 	int COUNT;
-	unsigned long START[MAX_SCHD_RECORDS];
-	unsigned long DUR[MAX_SCHD_RECORDS];
+	unsigned __int64
+		DATETIME[MAX_SCHD_RECORDS],
+		REMAIN_TO_START,
+		REMAIN_TO_STOP,
+		RECORD;
+
+	unsigned long 
+		START[MAX_SCHD_RECORDS],
+		DUR[MAX_SCHD_RECORDS];
+
+	void sort_starts(void);
 
 	CSchdVtrRecIni(char* filename);
 	virtual ~CSchdVtrRecIni();
 
+	static unsigned __int64 get_curr_day_tc(void);
+	static unsigned __int64 get_next_day_tc(void);
+	static unsigned __int64 get_now_day_tc(void);
 private:
-	void add_timecodes(unsigned long start_frm, unsigned long dur_frm);
+	void add_timecodes(unsigned long start_frm, unsigned long dur_frm, unsigned __int64 dur_abs_frm);
 };
 
 #endif // !defined(AFX_SCHDVTRRECINI_H__7E037EB8_5AB7_400A_B482_A43F47ACE138__INCLUDED_)
